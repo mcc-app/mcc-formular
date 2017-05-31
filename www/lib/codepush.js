b32e1da2db4c736fbae864b71bca1127460fadcd
PeBu <PeBu>
2017-05-31 10:45:42 +0200

    Version 1.0.3

--- www/lib/codepush.js
+++ www/lib/codepush.js
@@ -43,12 +43,14 @@ var cp = {
     },
     // Update DOM on a Received Event
     receivedEvent: function (id) {
+        /*
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+        */
 
         console.log('Received Event: ' + id);
     },
@@ -120,4 +122,3 @@ var cp = {
     }
 };
 
-cp.initialize();
