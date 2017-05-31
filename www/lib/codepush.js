d0c0e9ea1c552b23ae305dc8b02a18de3bfd5aab
PeBu <PeBu>
2017-05-31 10:25:52 +0200

    Version 1.0.3

--- www/lib/codepush.js
+++ www/lib/codepush.js
@@ -1,85 +1,16 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 
+var cp = {
 
-function syncStatus(status) {
-    switch (status) {
-        
-        case SyncStatus.UP_TO_DATE:
-            alert("UP_TO_DATE ccc");
-            // Show "downloading" modal
-            break;
-        case SyncStatus.UPDATE_INSTALLED:
-            alert("UPDATE_INSTALLED");
-            // Hide "downloading" modal
-            break;
-        case SyncStatus.UPDATE_IGNORED:
-            alert("UPDATE_IGNORED");
-            // Show "downloading" modal
-            break;
-        case SyncStatus.ERROR:
-            alert("ERROR");
-            // Hide "downloading" modal
-            break;
-        case SyncStatus.IN_PROGRESS:
-            // Show "downloading" modal
-            alert("IN_PROGRESS");
-            break;
-        case SyncStatus.CHECKING_FOR_UPDATE:
-            alert("CHECKING_FOR_UPDATE ccc");
-            // Hide "downloading" modal
-            break;
-        case SyncStatus.AWAITING_USER_ACTION:
-            alert("AWAITING_USER_ACTION");
-            // Show "downloading" modal
-            break;
-        case SyncStatus.DOWNLOADING_PACKAGE:
-            alert("DOWNLOADING_PACKAGE");
-            // Show "downloading" modal
-            break;
-        case SyncStatus.INSTALLING_UPDATE:
-            // Hide "downloading" modal
-            alert("INSTALLING_UPDATE");
-            break;
-    }
-}
-
-function downloadProgress(d) {
-    if (d) {
-
-        document.getElementById("bytes").innerHTML = ((d.receivedBytes / d.totalBytes) * 100).toFixed(2) + "%";
-        // Update "downloading" modal with current download %
-        //console.log("Downloading " + downloadProgress.receivedBytes + " of " + downloadProgress.totalBytes);
-    }
-}
-
-
-var app2 = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
     },
+
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
@@ -87,18 +18,31 @@ var app2 = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function () {
+        // Migrate data from older versions
+        window.codePush.getCurrentPackage(function (currentPackage) {
+            // getCurrentPackage returns null if no update was installed (app store version)
+            if (currentPackage && currentPackage.isFirstRun) {
+                // First run after an update, migrate data
+                if (currentPackage.appVersion === "1.0.0") {
+                    // migrate data from store version to version 1.0.0
+                } else if (currentPackage.appVersion === "2.0.0") {
+                    // migrate data to version 2.0.0
+                }
+            }
+            
+            // continue application initialization
+            cp.receivedEvent('deviceready');
+            
+            // Wait for 5s after the application started and check for updates.
+            setTimeout(cp.checkAndInstallUpdates, 5000);
+            
+            // Notify the plugin that update succeeded.
+            window.codePush.notifyApplicationReady();
 
-        try {
-            codePush.sync(syncStatus, null, downloadProgress);
-        }
-        catch (err) {
-            alert(err.message)
-        }
-
-        app.receivedEvent('deviceready');
+        }, cp.getErrorHandler("Error while retrieving the current package."));
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -107,5 +51,73 @@ var app2 = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+    // Uses the CodePush service configured in config.xml to check for updates, prompt the user and install them.
+    checkAndInstallUpdates: function () {
+        
+        // Check the CodePush server for updates.
+        console.log("Checking for updates...");
+        window.codePush.checkForUpdate(cp.checkSuccess, cp.getErrorHandler("Checking for update failed."));
+    },
+    // Called after the CodePush server responded the checkForUpdate call
+    checkSuccess: function (remotePackage) {
+        if (!remotePackage) {
+            // A null remotePackage means that the server successfully responded, but there is no update available.
+            console.log("The application is up to date.");
+        }
+        else {
+            console.log("There is an update available. Remote package:" + JSON.stringify(remotePackage));
+                
+            // Called after the user confirmed or canceled the update 
+            function onConfirm(buttonIndex) {
+                switch (buttonIndex) {
+                    case 1:
+                        /* Install */
+                        console.log("Downloading package...");
+                        remotePackage.download(cp.onDownloadSuccess, cp.getErrorHandler("Downloading the update package failed."));
+                        break;
+                    case 2:
+                        /* Cancel */
+                        /* nothing to do */
+                        break;
+                }
+            }
+
+            // Ask the user if they want to download and install the update
+            navigator.notification.confirm(
+                'An update is available. Would you like to download and install it?',
+                onConfirm,
+                'Update'
+                ['Install', 'Cancel']);
+        }
+    },
+    // Called after an update package was downloaded sucessfully.
+    onDownloadSuccess: function (localPackage) {
+        console.log("Local package downloaded. Local package: " + localPackage.localPath);
+
+        var installCallback = function () {
+            console.log("Install succeeded");
+        };
+
+        console.log("Installing package...");
+        localPackage.install(installCallback, cp.getErrorHandler("Installation failed."), { installMode: InstallMode.IMMEDIATE });
+    },
+    // Returns an error handler that logs the error to the console and displays a notification containing the error message.
+    getErrorHandler: function (message) {
+        // Displays a dialog containing a message.
+        var displayErrorMessage = function (message) {
+            navigator.notification.alert(
+                message,
+                null,
+                'CodePush',
+                'OK');
+        };
+
+        return function (error) {
+            console.log(message + ":" + error.message);
+            displayErrorMessage(message + ":" + error.message);
+        }
     }
 };
+
+cp.initialize();
